9546244c42e0942bab272a8cde58aff4349bc74f
jumirgosain <jumir@h2otechonline.com>
2024-12-25 11:24:08 +0545

    feat: :tada: Test project setup

---
+++ src/pages/Insight.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Insight = () => {
+  return (
+    <div>
+      insight
+    </div>
+  )
+}
+
+export default Insight
---
+++ src/pages/Map.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Map = () => {
+  return (
+    <div>
+      Map page
+    </div>
+  )
+}
+
+export default Map
